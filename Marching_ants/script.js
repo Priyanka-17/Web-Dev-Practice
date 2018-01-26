1587f385f84a4667258e44f77988d6d1c554dfac
Priyanka Mitra <priyanka.103111@gmail.com>
2018-01-26 02:36:17 -0800

    Edit script.js

--- Marching_ants/script.js
+++ Marching_ants/script.js
@@ -1,1 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var antColony = document.querySelector('.ant-colony');
+
+  for (var i = 0; i < 10; i++) {
+    var antDiv = document.createElement('div');
+    antDiv.classList.add('ant');
+    antDiv.style.top = i * 20 + 'px';
+    antDiv.style.left = i * 20 + 'px';
+    antColony.appendChild(antDiv);
+  }
+
+  var ants = document.querySelectorAll('.ant')
+  setInterval(function() {
+  moveAnts()}, 400);
+
+  function moveAnts() {
+    for (i = 0; i < ants.length; i++) {
+      var top = i * 20 + 10 * Math.random();
+      var left = i * 20 + 10 * Math.random();
+      ants[i].style.top = top + 'px';
+      ants[i].style.left = left + 'px';
+    }
+  }
+});
+
+
 
